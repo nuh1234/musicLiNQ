d6c0ec986755d245d46f7af1b6f8be7be9fe8f01
Nuh Mohamud <nuhmohamud@Nuhs-MacBook-Pro.local>
2018-02-17 18:38:00 -0800

    more LINQ queries

--- Program.cs
+++ Program.cs
@@ -9,25 +9,27 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            //Collections to work with
             List<Artist> Artists = JsonToFile<Artist>.ReadJson();
             List<Group> Groups = JsonToFile<Group>.ReadJson();
-            String key = "Mount Vernon";
+            String key = "William";
             //========================================================
-            //Solve all of the prompts below using various LINQ queries
+            //Solved all of the prompts below using various LINQ queries
             //========================================================
 
             //There is only one artist in this collection from Mount Vernon, what is their name and age?
             IEnumerable<Artist> found = Artists.Where(artist => artist.Hometown == key);
             foreach (Artist i in found) {
-                Console.WriteLine("{0} is from {1}", i.ArtistName, i.Hometown.ToString());
+                Console.WriteLine("Match found for hometown {0} - {1}", key, i.Hometown.ToString());
             }
 
             //Who is the youngest artist in our collection of artists?
             Artist youngest = (from match in Artists orderby match.Age ascending select match).First();
             Console.WriteLine("Youngest at {0} is {1}", youngest.Age, youngest.ArtistName);
             //Display all artists with 'William' somewhere in their real name
-
+            IEnumerable<Artist> matches = Artists.Where(match => match.RealName.Contains(key));
+            foreach (Artist i in matches) {
+                Console.WriteLine("Match found for real name {0} - {1}", key, i.RealName);
+            }
             //Display the 3 oldest artist from Atlanta
 
             //(Optional) Display the Group Name of all groups that have members that are not from New York City
