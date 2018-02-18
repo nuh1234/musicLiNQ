fd33fe1aaa46ea5ab019c35a1ee78c5c49bf0828
Nuh Mohamud <nuhmohamud@Nuhs-MacBook-Pro.local>
2018-02-17 20:54:18 -0800

    more class constants

--- Program.cs
+++ Program.cs
@@ -11,24 +11,24 @@ namespace ConsoleApplication
         {
             List<Artist> Artists = JsonToFile<Artist>.ReadJson();
             List<Group> Groups = JsonToFile<Group>.ReadJson();
-            const String key = "William";
+            const String KEY = "William";
             //========================================================
             //Solved all of the prompts below using various LINQ queries
             //========================================================
 
             //There is only one artist in this collection from Mount Vernon, what is their name and age?
-            IEnumerable<Artist> found = Artists.Where(artist => artist.Hometown == key);
+            IEnumerable<Artist> found = Artists.Where(artist => artist.Hometown == KEY);
             foreach (Artist i in found) {
-                Console.WriteLine("Match found for hometown {0} - {1}", key, i.Hometown.ToString());
+                Console.WriteLine("Match found for hometown {0} - {1}", KEY, i.Hometown.ToString());
             }
 
             //Who is the youngest artist in our collection of artists?
             Artist youngest = (from match in Artists orderby match.Age ascending select match).First();
             Console.WriteLine("Youngest at {0} is {1}", youngest.Age, youngest.ArtistName);
             //Display all artists with 'William' somewhere in their real name
-            IEnumerable<Artist> matches = Artists.Where(match => match.RealName.Contains(key));
+            IEnumerable<Artist> matches = Artists.Where(match => match.RealName.Contains(KEY));
             foreach (Artist i in matches) {
-                Console.WriteLine("Match found for real name {0} - {1}", key, i.RealName);
+                Console.WriteLine("Match found for real name {0} - {1}", KEY, i.RealName);
             }
             //Display the 3 oldest artist from Atlanta
 
